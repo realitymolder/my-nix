0baff7e62206593413767473e808378463b178ed
Daniel Toubul <molder@toubul.eu>
2026-03-22 04:55:13 +0100

    nix hardware flake added inside file

--- modules/hosts/samson.nix
+++ modules/hosts/samson.nix
@@ -1,5 +1,8 @@
-{ den, ... }:
+{ inputs, den, ... }:
 {
+    flake-file.inputs.opencode.url = "github:NixOS/nixos-hardware";
+
+
   den.hosts.x86_64-linux.samson.users.daniel = { };
 
   den.aspects.samson = {
@@ -13,9 +16,46 @@
     ];
 
     nixos =
-      { pkgs, ... }:
+      { pkgs, lib, ... }:
       {
-        environment.systemPackages = [ pkgs.hello ];
+        
+        imports = [
+          inputs.nixos-hardware.nixosModules.common-cpu-amd
+          inputs.nixos-hardware.nixosModules.common-gpu-amd
+        ];
+
+        # File systems based on nixos-generate-config output
+        fileSystems."/boot" = {
+          device = "/dev/disk/by-uuid/XXX"; # Replace with actual UUID
+          fsType = "vfat";
+        };
+
+        fileSystems."/" = {
+          device = "/dev/disk/by-uuid/XXX"; # Replace with actual UUID
+          fsType = "ext4";
+        };
+
+        swapDevices = [
+          {
+            device = "/swapfile";
+            size = 65536;
+          }
+        ];
+
+        services.xserver = {
+          videoDrivers = [ "amdgpu" ];
+          deviceSection = lib.mkDefault ''
+            Option "TearFree" "true"
+          '';
+        };
+
+        hardware.graphics = {
+          enable = true;
+          extraPackages = with pkgs; [
+            mesa
+            vulkan-loader
+          ];
+        };
       };
 
     homeManager =
